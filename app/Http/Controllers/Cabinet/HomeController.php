93b7ca53dfeb20754e6e0fcec8d925960d314383
Artem Zinoviev <arzinoviev@gmail.com>
2018-04-27 19:22:45 +0300

    separate user view from guest

---
+++ app/Http/Controllers/Cabinet/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Board\Http\Controllers\Cabinet;
+
+use Illuminate\Routing\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('cabinet.home');
+    }
+}
