2f96f1a9b7dcc49ec9101df3ccf0d039835b4267
Artem Zinoviev <arzinoviev@gmail.com>
2018-05-07 18:19:38 +0300

    remove unused code, add editable field

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -24,14 +24,14 @@
         </div>
 
         <div class="form-group">
-            <label for="role" class="col-form-label">Role</label>
-            <select id="role" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="role">
-                @foreach ($roles as $value => $label)
-                    <option value="{{ $value }}"{{ $value === old('role', $user->role) ? ' selected' : '' }}>{{ $label }}</option>
+            <label for="status" class="col-form-label">Status</label>
+            <select id="status" type="email" class="form-control{{ $errors->has('status') ? ' is-invalid' : '' }}" name="status">
+                @foreach ($statuses as $value => $label)
+                    <option value="{{ $value }}"{{ $value === old('status', $user->status) ? ' selected' : '' }}>{{ $label }}</option>
                 @endforeach;
             </select>
-            @if ($errors->has('role'))
-                <span class="invalid-feedback"><strong>{{ $errors->first('role') }}</strong></span>
+            @if ($errors->has('email'))
+                <span class="invalid-feedback"><strong>{{ $errors->first('status') }}</strong></span>
             @endif
         </div>
 
