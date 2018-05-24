373e7a695d8dc1c661b0ec027a3fbafdd1f83b04
Artem Zinoviev <arzinoviev@gmail.com>
2018-05-24 17:00:46 +0300

    add nested sets lib; add adverts database+controller+model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,5 +8,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UsersTableSeeder::class);
          $this->call(RegionsTableSeeder::class);
+         $this->call(AdvertsCategoriesTableSeeder::class);
     }
 }
