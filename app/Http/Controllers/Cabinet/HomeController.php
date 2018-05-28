859c795f4de47f23c384505a575756b0f69dde7a
Artem Zinoviev <arzinoviev@gmail.com>
2018-05-28 15:18:23 +0300

    fix controllers

--- app/Http/Controllers/Cabinet/HomeController.php
+++ app/Http/Controllers/Cabinet/HomeController.php
@@ -2,15 +2,10 @@
 
 namespace Board\Http\Controllers\Cabinet;
 
-use Illuminate\Routing\Controller;
+use Board\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         return view('cabinet.home');
